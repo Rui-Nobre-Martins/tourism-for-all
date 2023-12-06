4f0202433417a51ef53a1fcc1cdd60cc467eeeff
Rui  Martins <>
2023-12-06 20:07:53 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+  
+  return (
+    <>
+    <header>
+      
+      <nav>
+        <img src="" alt="logo" />
+        <a href="/">Home</a>
+        <a href="/addlocation">Add Location</a>
+        <a href="/thetraveler">The Traveler</a>
+      </nav>
+    </header>
+      
+    </>
+  )
+}
+
+export default App
