566b9ca2fb13e27dfb0c9214dad6132efa77334d
Rui  Martins <>
2023-12-07 23:10:27 +0000

    navbar almost done

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,44 @@ function App() {
   
   return (
     <>
-    
+    {/* Start Navegation Bar */}
+
     <nav className="navBar">
-      <img src="" alt="logo" />
-      <a href="/">Home</a>
-      <a href="/addlocation">Add Location</a>
-      <a href="/thetraveler">The Traveler</a>
+      <div className="navLogo">
+        <a href="/">Travel For All</a> 
+      </div>
+      <ul className="navLink">
+        <li><a href="/">Home</a></li>
+        <li><a href="/addlocation">Add Location</a></li>
+        <li><a href="/thetraveler">The Traveler</a></li>
+      </ul>
+      <button className="btnContact">Contacts</button>
     </nav>
-  
-    <header>
-      <h1>Travel For All</h1>
-      <img src="../src/assets/header.jpg" alt="" />
+
+    {/* Finish Favegation Bar */}
+
+    
+     {/* Start Header */}
+
+    <header className="headerContainer">
+        <h1 className="headerTitle">Dream Travel</h1>
+        <img className="headerImage" src="../src/assets/header.jpg" alt="Plane lading" />
     </header>
+
+    {/* Finish Header */}
+
+    {/* Start Container with latest trips */}
+    <section>
+      <h2>Latest</h2>
+      <div className="travelContainer">
+        <div className="travelCard">
+          <div className="travelCardContent"></div>
+        </div>
+        
+      </div>
+    </section>
+    {/* Finish Container with latest trips */}
+
 
 
 
