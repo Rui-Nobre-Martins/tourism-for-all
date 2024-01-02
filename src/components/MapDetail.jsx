867927686da69ff5711746f803e26a57678f7e99
Rui  Martins <>
2024-01-02 19:01:49 +0000

    no message

---
+++ src/components/MapDetail.jsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import "mapbox-gl/dist/mapbox-gl.css";
+import  Map,{ NavigationControl, Marker} from 'react-map-gl';
+
+function MapDetail ({cityInfo}) {
+    
+    const MapDetailTOKEN = "pk.eyJ1Ijoicm1hcnRpbnMwMSIsImEiOiJjbHFsank3dHYxNTRxMmlyeTE5aTRsdmJzIn0.ru0GQjaXB_qaVMJWez801g";
+
+    const lat = cityInfo.lat;
+    const lon = cityInfo.lon;
+    
+    return(
+        <>
+           <div className="mapDetail">
+                <Map style={{width: "100%", height:"500px"}}
+
+                mapboxAccessToken= {MapDetailTOKEN}
+
+                initialViewState={{
+
+                        latitude: lat,
+                        longitude: lon,
+                        zoom: 1
+                    }}
+                    mapStyle="mapbox://styles/mapbox/streets-v9">
+                        <Marker latitude={cityInfo.lat} longitude={cityInfo.lon}></Marker>
+                        <NavigationControl/>
+                </Map>
+            </div> 
+        </>
+    )}
+
+export default MapDetail;
