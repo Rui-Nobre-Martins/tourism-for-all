fa0033c18ccf2f74f9bfc3e0724411042877c409
Rui  Martins <>
2024-01-06 11:13:25 +0000

    footer icon done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,10 @@ function Footer () {
         <>
         <footer>
             <div className="footer">
-                <p>Copyright</p>
-                <p>Social Media ICON</p>
-                <p>HELP!!!FOOTER</p>
+                <p>© Rui Nobre Martins</p>
+                <a href="https://www.instagram.com/ruinobremartins/"><img className="iconFooter" src="/image/square-instagram.svg" alt="Instagram Icon" /></a>
+                <a href="https://github.com/Rui-Nobre-Martins"><img className="iconFooter" src="/image/square-github.1.svg" alt="GitHub Icon" /></a>
+                <a href="https://www.linkedin.com/in/rui-nobre-martins-29217bba/"><img className="iconFooter" src="/image/linkedin.svg" alt="Linkedin Icon" /></a>
             </div>
         </footer>
         </>
