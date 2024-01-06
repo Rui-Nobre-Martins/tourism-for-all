cabc975d5fb240fadea5cd02d1baf7ccd251f210
Rui  Martins <>
2024-01-06 11:49:17 +0000

    no message

--- src/components/NavegationBar.jsx
+++ src/components/NavegationBar.jsx
@@ -8,15 +8,31 @@ function NavegationBar() {
                 <div className="navLogo">
                     <Link href="/"><img src="/image/favicon.png" alt="logo"/></Link>
                 </div>
+
             <div className="navLink">
-                <a href="/"></a>
+                
                 <Link href="/">
-                    Home</Link>
-                {/* <Link href="/thetraveler">The Traveler</Link> */}
-                <Link href="/addlocation">Add Location</Link>
-                <Link href="/aboutus">About us</Link>
+                    <img src="/image/house-solid.svg" alt="Home Button" /> 
+                    <span className="hideLetter">Home</span> 
+                </Link>
+                
+                <Link href="/addlocation"> 
+                    <img src="/image/map-pin-solid.svg" alt="Add location" /> 
+                    <span className="hideLetter">Add Location</span> 
+                </Link>
+
+                <Link href="/aboutus"> 
+                    <img src="/image/user-solid.svg" alt="" />  
+                    <span className="hideLetter">About us</span> 
+                </Link>
             </div>
             </nav>
+        </div>
+
+        <div className="mobileIcon">
+            <img src="/image/house-solid.svg" alt="Home Button" /> 
+            <img src="/image/map-pin-solid.svg" alt="Add location" /> 
+            <img src="/image/user-solid.svg" alt="" />  
         </div>
 
         </>
