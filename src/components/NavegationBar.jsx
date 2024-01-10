7ec6e9e1a53cf533242edf0df14e2d9648d79c7f
Rui  Martins <>
2024-01-10 21:29:32 +0000

    navBar version Mobile Done

--- src/components/NavegationBar.jsx
+++ src/components/NavegationBar.jsx
@@ -9,32 +9,43 @@ function NavegationBar() {
                     <Link href="/"><img src="/image/favicon.png" alt="logo"/></Link>
                 </div>
 
-            <div className="navLink">
+                <div className="navLink">
                 
                 <Link href="/">
-                    <img src="/image/house-solid.svg" alt="Home Button" /> 
+                    <img className="hideIcon" src="/image/house-solid.svg" alt="Home Button" /> 
                     <span className="hideLetter">Home</span> 
                 </Link>
                 
                 <Link href="/addlocation"> 
-                    <img src="/image/map-pin-solid.svg" alt="Add location" /> 
-                    <span className="hideLetter">Add Location</span> 
+                    <img className="hideIcon" src="/image/map-pin-solid.svg" alt="Add location" /> 
+                    <span className="hideLetter">Add Location</span>
                 </Link>
 
                 <Link href="/aboutus"> 
-                    <img src="/image/user-solid.svg" alt="" />  
+                    <img className="hideIcon" src="/image/user-solid.svg" alt="" />  
                     <span className="hideLetter">About Me</span> 
                 </Link>
+                </div>
+
+            <div className="mobileIcon">
+                <Link href="/">
+                <img className="navMobileIcon" src="/image/house-solid.svg" alt="Home Button" /> 
+                </Link>
+                <Link href="/addlocation">
+                <img className="navMobileIcon" src="/image/map-pin-solid.svg" alt="Add location" />
+                </Link> 
+                <Link href="/aboutus">
+                <img className="navMobileIcon" src="/image/user-solid.svg" alt="" />
+                </Link>
             </div>
+
             </nav>
+
+            
         </div>
 
-        {/* <div className="mobileIcon">
-            <img src="/image/house-solid.svg" alt="Home Button" /> 
-            <img src="/image/map-pin-solid.svg" alt="Add location" /> 
-            <img src="/image/user-solid.svg" alt="" />  
-        </div> */}
-
+        
+        
         </>
     )
 }
