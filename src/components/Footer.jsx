28dd01f3c2d2d2394eeb8ed1650946a996aa7a2b
Rui  Martins <>
2024-01-15 18:47:18 +0000

    no message

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,7 @@ function Footer () {
                 <a href="https://www.instagram.com/ruinobremartins/"><img className="iconFooter" src="/image/square-instagram.svg" alt="Instagram Icon" /></a>
                 <a href="https://github.com/Rui-Nobre-Martins"><img className="iconFooter" src="/image/square-github.1.svg" alt="GitHub Icon" /></a>
                 <a href="https://www.linkedin.com/in/rui-nobre-martins-29217bba/"><img className="iconFooter" src="/image/linkedin.svg" alt="Linkedin Icon" /></a>
+                <p>Images used on this website are for illustrative purposes only and all credits belong to their original authors.</p>
             </div>
         </footer>
         </>
